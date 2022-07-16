0dfa3110d306971e3a03160d3a579a180897bb41
amanat <text7434@gmail.com>
2022-07-16 13:52:38 +0500

    added Day model class and wrote Recyclerview Adapter

---
+++ app/src/main/java/com/example/fitapp/adapters/DaysAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.fitapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitapp.R
+import com.example.fitapp.databinding.DaysListItemBinding
+
+class DaysAdapter :ListAdapter<DayModel, DaysAdapter.DayHolder> {
+
+    class DayHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = DaysListItemBinding.bind(view)
+        fun setData(day: DayModel) = with(binding){
+            val name = root.context.getString(R.string.day) + " ${adapterPosition + 1}"
+            tvName.text = name
+            val exCounter = day.exercises.split(",").size.toString()
+            tvCounter.text = exCounter
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayHolder {
+        val view = LayoutInflater.from(parent.context).
+        inflate(R.layout.days_list_item, parent, false)
+        return  DayHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DayHolder, position: Int) {
+        holder.setData(getItem(position))
+    }
+    class MyComparator : DiffUtil.ItemCallback<DayModel>(){
+        override fun areItemsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+}
