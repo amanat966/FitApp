8ba72976f7f08e43fa991f838de4f8a94d597ba0
amanat966 <text7434@gmail.com>
2022-08-30 21:15:47 +0600

    solve problems

--- app/src/main/java/com/example/fitapp/fragments/DaysFragment.kt
+++ app/src/main/java/com/example/fitapp/fragments/DaysFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitapp.R
 import com.example.fitapp.adapters.DayModel
+import com.example.fitapp.adapters.DaysAdapter
 import com.example.fitapp.databinding.FragmentDaysBinding
 
 
@@ -20,6 +23,17 @@ class DaysFragment : Fragment() {
     ): View? {
         binding = FragmentDaysBinding.inflate(inflater,container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initReview()
+    }
+    private fun initReview() = with(binding) {
+        val adapter = DaysAdapter()
+        rcViewDays.layoutManager = LinearLayoutManager(activity as AppCompatActivity)
+        rcViewDays.adapter = adapter
+        adapter.submitList(fillDaysArray())
     }
 
     private fun fillDaysArray(): ArrayList<DayModel>{
