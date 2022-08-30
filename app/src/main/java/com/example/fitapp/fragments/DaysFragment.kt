94b324c1f876589f46ceac9d08604781937eb0a7
amanat966 <text7434@gmail.com>
2022-08-30 21:25:57 +0600

    reset project

--- app/src/main/java/com/example/fitapp/fragments/DaysFragment.kt
+++ app/src/main/java/com/example/fitapp/fragments/DaysFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitapp.R
+import com.example.fitapp.adapters.DayModel
+import com.example.fitapp.adapters.DaysAdapter
 import com.example.fitapp.databinding.FragmentDaysBinding
 
 
@@ -21,6 +25,24 @@ class DaysFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initReview()
+    }
+    private fun initReview() = with(binding) {
+        val adapter = DaysAdapter()
+        rcViewDays.layoutManager = LinearLayoutManager(activity as AppCompatActivity)
+        rcViewDays.adapter = adapter
+        adapter.submitList(fillDaysArray())
+    }
+
+    private fun fillDaysArray(): ArrayList<DayModel>{
+        val tArray = ArrayList<DayModel>()
+        resources.getStringArray(R.array.day_exercises).forEach {
+            tArray.add(DayModel(it, false))
+        }
+        return tArray
+    }
     companion object {
 
         @JvmStatic
